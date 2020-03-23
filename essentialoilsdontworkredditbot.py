5729e5ff65a159353b7beba30bb4a4fb70c805ef
Chris <cvila17@yandex.com>
2020-03-23 14:42:18 -0400

    Update essentialoilsdontworkredditbot.py

--- essentialoilsdontworkredditbot.py
+++ essentialoilsdontworkredditbot.py
@@ -8,11 +8,10 @@ reddit = praw.Reddit(client_id = '',
 subreddit = reddit.subreddit('all')
 
 keyphrase1 = 'essential oils work '
-keyphrase2 = 'they do work '
-keyphrase3 = 'it does work '
+keyphrase2 = 'essential oils really do work '
 
 for comment in subreddit.stream.comments():
-    if (keyphrase1 in comment.body) or (keyphrase2 in comment.body) or (keyphrase3 in comment.body):
+    if (keyphrase1 in comment.body) or (keyphrase2 in comment.body):
         try:
             comment_reply = 'No they don\'t honey please check out *https://www.huffpost.com/entry/essential-oils_l_5d93a00de4b0019647b010df*'
             comment.reply(comment_reply)
